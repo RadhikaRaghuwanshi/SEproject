7de578e042f905489c55cdb7a84b292a8c057821
sumeet sapla <sumeet sapla>
2017-04-20 10:04:37 +0530

    Second Version

--- comment.inc.php
+++ comment.inc.php
@@ -17,12 +17,12 @@ function getComment($conn)
 	$result = $conn->query($sql);
 	while($row = $result->fetch_assoc()){
 		$id = $row['uid'];
-		$sql2 = "SELECT * FROM user WHERE id ='$id'";
+		$sql2 = "SELECT * FROM signup WHERE id ='$id'";
 	    $result2 = $conn->query($sql2);
 	    if($row2 = $result2->fetch_assoc())
 	    {
           echo "<div class='comment-box'><p>";
-          echo  $row2['uid']."<br>";
+          echo  $row2['user']."<br>";
           echo  $row['date']."<br>"; 
           echo nl2br($row['message']) ;
           echo "</p></div>";
@@ -34,19 +34,19 @@ function getComment($conn)
 function getLogin($conn)
 { 
     if(isset($_POST['loginSubmit'])){
-    $uid = $_POST['uid'];
-    $pwd = $_POST['pwd'];
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
 
-	$sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='$pwd'";
+	$sql = "SELECT * FROM signup WHERE user='$user' AND pass='$pass'";
 	$result = $conn->query($sql);
 	if(mysqli_num_rows($result) > 0){
 		if($row = $result->fetch_assoc()){
 	       $_SESSION['id'] = $row['id'];
-	       header("Location: index.php?loginsuccess");
+	       header("Location:index.php?loginsuccess");
 	       exit();
 	   }
 	}else{
-		 header("Location: index.php?loginfailed");
+		 header("Location: Loginform.php?loginfailed");
 	       exit();
 
 	}
@@ -62,3 +62,28 @@ function  userlogout($conn){
 	}
   
 }
+
+
+function getusername($conn){
+
+
+$query = "SELECT * FROM signup WHERE id ='".$_SESSION['id']."' ";
+$result = $conn->query($query);
+
+while($row = $result->fetch_assoc())
+{
+  
+  echo  "<center><h2>Hello ".$row['user']." Submit your Report</h2></center>";
+
+}
+
+
+}
+
+
+
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -11,51 +11,64 @@
 <head>
 <meta charset="UTF-8">
 <title>Title of the document</title>
-<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="indexstyle2.css">
+<link rel="stylesheet" type="text/css" href="indexstyle1.css">
+
+
+
+
 </head>
 
 <body>
 
-<?php
-    echo "<form method='POST' action='".getLogin($conn)."' >
-    <input type='text' name='uid'>
-    <input type='password' name='pwd'>
-    <button type='submit' name='loginSubmit'>Login</button>
-</form>";
-   echo "<form method='POST' action='".userLogout($conn)."' >
-     <button type='submit' name='logoutSubmit'>Logout</button>
-</form>";
+<div class="img">
+<ul>
+  <li><a href="home.html">Home</a></li>
+  <li><a href="Loginform.php">Login</a></li>
+  <li><a href="image-upload.php">Upload</a></li>
+  <li><a href="fb.php">Feedback</a></li>
+ 
+</ul> 
 
-       if(isset($_SESSION['id'])){
-	          echo "You are logged in!";
-          }else{
-            	echo "You are NOT logged in!";
-         }
-?>
+<br>
 
-<br><br>
 
-<iframe src="http://mmtuts.net/course.php?c=php&l=8" frameborder="0" allowfullscreen></iframe>
+  <iframe id="airpano_frame" width="500" height="450"
+  src="http://www.click360.in/virtualtour/lordganeshfestival/lalbaugcharaja/2016/lalbaugcharaja2016.html" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" framespacing="0"> </iframe>
 <br><br>
 
 <?php
 
        if(isset($_SESSION['id'])){
-	          echo "<form method='POST' action='".setComment($conn)."'>
+            echo "<form method='POST' action='".setComment($conn)."'>
                 <input type='hidden' name='uid' value='".$_SESSION['id']."'>
                  <input type='hidden' name='date' value='".date('y-m-d H:i:s')."'>
                 <textarea name='message'></textarea><br>
                 <button type='submit' name='commentSubmit'>Comment</button>
              </form>";
           }else{
-            	echo "You need to be logged in to comment!
-            	<br><br>";
+              echo "You need to be logged in to comment!
+              <br>";
          }
  
-
-getComment($conn);
 ?>
 
+  <div class="desc">
+  <form id="report" action="loginvalidation.php">
+  <button id="myBtn" type="submit" >Report</button>
+  </form>
+  </div>
+
+
+<?php
+getComment($conn);
+?>
+</div>
+
+
+ 
+
+<br><br>
 
 </body>
 </html>
